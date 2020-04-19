40d271b01c49dab9aed29ad32f336d33ef3003d1
Arylmera <guillaume@lemer.be>
2020-04-19 15:18:03 +0200

    clean code and dependancies

--- JavaInterface/src/pic/Gui.java
+++ JavaInterface/src/pic/Gui.java
@@ -5,27 +5,27 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Gui extends JFrame {
-	
+
 	private JLabel labelEntreeSonde;
 	private JButton boutonEntreeSonde;
 	private JTextField texteEntreeSonde;
 	private static double valeurSonde = 0.0;
-	
-	
+
+
 	public static double getValeurSonde() {
 		return Gui.valeurSonde;
 	}
-	
+
 	public static void setValeurSonde(double valeur) {
 		Gui.valeurSonde = valeur;
 	}
-	
+
 	public Gui() {
 		setLayout(new FlowLayout());
-		
+
 		labelEntreeSonde = new JLabel("Distance avant alerte");
 		add(labelEntreeSonde);
-		
+
 		texteEntreeSonde = new JTextField(10);
 		add(texteEntreeSonde);
 		// key listener pour empécher tout caractère sauf les numéros, le backspace et la virgule (numérique)
@@ -37,30 +37,29 @@ public class Gui extends JFrame {
 				}
 			}
 		});
-		
-		
+
 		boutonEntreeSonde = new JButton("Confirmer");
 		add(boutonEntreeSonde);
 		boutonEntreeSonde.addActionListener(new ActionListener(){
-			   public void actionPerformed(ActionEvent ae){
-			      valeurSonde = Double.parseDouble(texteEntreeSonde.getText());
-			   }
-			});
+			public void actionPerformed(ActionEvent ae){
+				valeurSonde = Double.parseDouble(texteEntreeSonde.getText());
+			}
+		});
 	}
-	
+
 	public static void Fenetre() {
-		 Gui fenetre = new Gui();
-		 fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 fenetre.setTitle("Projet Electronique Digitale");
-		 fenetre.setSize(600,400);
-		 fenetre.setLocationRelativeTo(null);
-		 fenetre.setVisible(true);
+		Gui fenetre = new Gui();
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.setTitle("Projet Electronique Digitale");
+		fenetre.setSize(600,400);
+		fenetre.setLocationRelativeTo(null);
+		fenetre.setVisible(true);
 	}
-	
+
 	public static void main(String args[]) {
 		Fenetre();
 	}
-	
-	
+
+
 }
 
