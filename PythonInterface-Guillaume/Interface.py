7a839d1db52533b45ae6dc620fcb326b96e21612
Arylmera <guillaume@lemer.be>
2020-05-03 14:18:42 +0200

    python read OK

--- PythonInterface-Guillaume/Interface.py
+++ PythonInterface-Guillaume/Interface.py
@@ -1,56 +1,52 @@
 import time
 import serial
+#from termcolor import colored, cprint
 
 serPort = 'COM2'
 baudRate = 9600
 
-serial = serial.Serial(serPort, baudRate, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=0)
+serial = serial.Serial(serPort, baudRate)
 
 '''
 Lecture des valeur du port serial
 Print de la valeur recue
 '''
 def readValue() :
-    line = serial.readline()   # read a '\n' terminated line
-    value = int(line)
-    if (value == 1) :
-        print('OK \n')
-    elif (value == 2) :
-        print('Alerte \n')
+    line = (serial.readline()).decode()   # read a '\n' terminated line
+    line = line[:-1]
+    if (line == '1') :
+        print('OK')
+        #cprint("OK", 'green')
+    elif (line == '2') :
+        print('Alerte')
+        #cprint("Alerte", 'red')
     else :
-        print('distance de : '+ value + ' cm \n')
-
-'''
-Envois de la valeur max a la sonde
-'''
-def sendMaxValue(value):
-    print('envois de la valuer ' + value + ' a la pic comme valeur minimum \n')
-    time.sleep(1)
-    line = serial.writelines('!'+ value)
-    return line
-
-'''
-Fonctione principale de gestion du programme
-'''
-def main():
-    print("Entré la valeur minimum de distance de la sonde")
-    value = input()
-    sendMaxValue(value)
-    print("Valeur" + value + "envoyée comme valeur minimum \n")
-
-    try :
-        while 1:
-            if (serial.inWaiting() > 0):
-                readValue()
-
-            time.sleep(1)
-    except KeyboardInterrupt :
-        pass
-
-    serial.close()
+        print('distance de : '+ line + ' cm')
+    serial.flush()
 
 
 '''
 Lancement du programme
 '''
-main()
+try:
+    print("Ouverture du port COM2")
+    serial.close()
+    serial.open()
+    print("Port serial COM2 ouvert \n")
+    print("Entré la valeur minimum d'alerte pour la sonde")
+    minValue = input()
+    print("Envois de "+ minValue +" comme valeur minimum")
+    serial.writelines( ('!'+ minValue).encode() )
+
+    time.sleep(2)
+    try:
+        print("Appuyez sur une touche pour terminer")
+        while 1:
+            if (serial.inWaiting() > 0):
+                readValue()
+
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+finally:
+    serial.close()
